6d07c6eb8083e2165635b9bd37c45e78a711929f
theamallalgi <amallalgi2487@gmail.com>
2024-11-06 23:36:09 +0530

    change hi color of indent char

--- nvim/lua/amal/plugins/indentblankline.lua
+++ nvim/lua/amal/plugins/indentblankline.lua
@@ -1,18 +1,18 @@
 return {
-  "lukas-reineke/indent-blankline.nvim",
-	lazyload=true,
-  main = "ibl",
-  opts = {},
-  config = function()
-    require('ibl').setup({
-      scope = {
-        enabled = true, -- enable scope highlighting
-        show_start = false, -- don't show the start of the scope
-        show_end = false, -- don't show the end of the scope
-      },
-      indent = {
-        char = '|', -- use '|' for indentation character
-      },
-    })
-  end
+	"lukas-reineke/indent-blankline.nvim",
+	lazyload = true,
+	main = "ibl",
+	opts = {},
+	config = function()
+		require("ibl").setup({
+			scope = {
+				enabled = true, -- enable scope highlighting
+				show_start = false, -- don't show the start of the scope
+				show_end = false, -- don't show the end of the scope
+			},
+			indent = {
+				char = "|", -- use '|' for indentation character
+			},
+		})
+	end,
 }
