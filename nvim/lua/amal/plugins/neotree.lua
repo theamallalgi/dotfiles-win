979a3fa5fd041b25f11e5608d606bb96939f1b2a
theamallalgi <amallalgi2487@gmail.com>
2024-11-09 21:28:16 +0530

    setup more options/config

--- nvim/lua/amal/plugins/neotree.lua
+++ nvim/lua/amal/plugins/neotree.lua
@@ -15,26 +15,59 @@ return {
 			end,
 		})
 	end,
-	opts = {
-		hide_root_node = true,
-		retain_hidden_root_indent = true,
-		popup_border_style = "rounded",
-		filesystem = {
-			bind_to_cwd = true,
-			follow_current_file = { enabled = true },
-			use_libuv_file_watcher = true,
-			hijack_netrw_behavior = "open_current",
-			cwd_target = "current", -- Ensures Neo-tree always opens in the current working directory
-			filtered_items = {
-				hide_dotfiles = false,
-				hide_by_name = { ".git", ".DS_Store" },
-				always_show = { ".env" },
-			},
-		},
-	},
+	opts = {},
 	dependencies = {
 		{ "nvim-lua/plenary.nvim", lazy = true },
 		{ "nvim-tree/nvim-web-devicons", lazy = true },
 		{ "MunifTanjim/nui.nvim", lazy = true },
 	},
+	config = function()
+		require("neo-tree").setup({
+			popup_border_style = "single",
+			enable_modified_markers = true,
+			default_component_configs = {
+				modified = {
+					symbol = "󰫢",
+					highlight = "NeoTreeModified",
+				},
+				icon = {
+					folder_closed = "󰉋",
+					folder_open = "󰝰",
+					folder_empty = "󰉖",
+					folder_empty_open = "󰷏",
+				},
+				git_status = {
+					symbols = {
+						added = "",
+						deleted = "",
+						modified = "󰏫",
+						renamed = "󰯁",
+						untracked = "",
+						ignored = "󰈉",
+						unstaged = "",
+						staged = "",
+						conflict = "",
+					},
+				},
+			},
+			window = {
+				position = "left",
+				width = 35,
+			},
+			hide_root_node = true,
+			retain_hidden_root_indent = true,
+			filesystem = {
+				bind_to_cwd = true,
+				follow_current_file = { enabled = true },
+				use_libuv_file_watcher = true,
+				hijack_netrw_behavior = "open_current",
+				cwd_target = "current", -- Ensures Neo-tree always opens in the current working directory
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_by_name = { ".git", ".DS_Store" },
+					always_show = { ".env" },
+				},
+			},
+		})
+	end,
 }
