686a91e97fad4405883452e3cb68d75b9a96e433
Amal Lalgi <amallalgi2487@gmail.com>
2024-12-07 00:57:11 +0530

    feat(plugin): add snacks.nvim plugin
    use minimal configurations
    
    NOTE: TESTING PERIOD

---
+++ nvim/lua/amal/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		bigfile = { enabled = true },
+		notify = { enabled = true },
+		notifier = { enabled = true },
+		quickfile = { enabled = true },
+		lazygit = { enabled = true },
+		rename = { enabled = true },
+		terminal = { enabled = true },
+	},
+}
