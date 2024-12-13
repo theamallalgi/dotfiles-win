b73ad89451653672beccc7e08dde75d8675d8b50
Amal Lalgi <amallalgi2487@gmail.com>
2024-12-13 18:06:19 +0530

    feat: add function to install required buckets

--- install/scoop.sh
+++ install/scoop.sh
@@ -11,6 +11,19 @@ install_or_update_scoop() {
     fi
 }
 
+# installs the required buckets
+install_buckets() {
+    local buckets=("$@")
+    for bucket in "${buckets[@]}"; do
+        if ! scoop bucket list | grep -q "$bucket"; then
+            echo "[!] bucket '$bucket' not found. Installing..."
+            scoop bucket add "$bucket"
+        else
+            echo "[i] bucket '$bucket' already installed."
+        fi
+    done
+}
+
 # install apps in a category
 install_apps() {
     local category=$1
@@ -30,7 +43,7 @@ install_apps() {
 
 # apps sorted in categories
 system_apps=(7zip vcredist2022 wget winrar-np powertoys)
-dev_tools=(git neovim neovide vscode sublime-text lazygit nvm nodejs python make mingw gcc sass)
+dev_tools=(git neovim neovide vscode sublime-text lazygit nvm nodejs python make mingw gcc sass grep ripgrep)
 browsers=(brave googlechrome firefox-nightly min)
 media_tools=(ffmpeg mpv)
 terminals=(alacritty wezterm windows-terminal-preview)
@@ -38,7 +51,11 @@ fonts=(JetBrains-Mono JetBrainsMono-NF JetBrainsMono-NF-Mono)
 utilities=(fzf bat fd eza lsd glow zoxide oh-my-posh tor chromedriver)
 other_apps=(libreoffice localsend telegram spotify notion xnview mp3tag winaero-tweaker mods rio ttyd charm-gum)
 
+# buckets sorted in a list
+buckets_to_install=("norportable" "scoop-nerd-fonts")
+
 install_or_update_scoop # ensure scoop is installed or updated
+install_buckets "${buckets_to_install[@]}" # install the specified buckets
 
 # Install apps by category
 install_apps "system apps" "${system_apps[@]}"
