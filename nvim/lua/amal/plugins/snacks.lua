540764a057800770b8f8cb3a516788323c6a0307
Amal Lalgi <amallalgi2487@gmail.com>
2024-12-17 21:43:25 +0530

    chore(plugin): add minor snacks configurations
    BREAKING CHANGE: change notifier timeout to 1 second

--- nvim/lua/amal/plugins/snacks.lua
+++ nvim/lua/amal/plugins/snacks.lua
@@ -3,12 +3,50 @@ return {
 	priority = 1000,
 	lazy = false,
 	opts = {
+		-- snacks.bigfile (faster when opening big files)
 		bigfile = { enabled = true },
+		-- snacks.notify (better notify)
 		notify = { enabled = true },
-		notifier = { enabled = true },
+		-- snacks.notifier (works best with notify)
+		notifier = {
+			enabled = true,
+			timeout = 1000,
+		},
+		styles = {
+			notification = {
+				wo = { wrap = false },
+			},
+		},
+		-- snacks.quickfile (makes opening empty files faster)
 		quickfile = { enabled = true },
+		-- snacks.lazygit (lazygit)
 		lazygit = { enabled = true },
+		-- snacks.rename (lsp rename)
 		rename = { enabled = true },
+		-- snacks.terminal (terminal toggle)
 		terminal = { enabled = true },
+	},
+	keys = {
+		{
+			"<leader>gg",
+			function()
+				Snacks.lazygit()
+			end,
+			desc = "Lazygit",
+		},
+		{
+			"<leader>n",
+			function()
+				Snacks.notifier.hide()
+			end,
+			desc = "Dismiss All Notifications",
+		},
+		{
+			"<c-/>",
+			function()
+				Snacks.terminal()
+			end,
+			desc = "Toggle Terminal",
+		},
 	},
 }
