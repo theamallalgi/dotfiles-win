112d84988b9fb7ed116a1c5bdcaa37f1005bc21c
Amal Lalgi <amallalgi2487@gmail.com>
2024-12-26 21:43:45 +0530

    feat: add function to disable and enable plugin on keymap press

--- nvim/lua/amal/plugins/copilot.lua
+++ nvim/lua/amal/plugins/copilot.lua
@@ -2,8 +2,27 @@ return {
 	"zbirenbaum/copilot.lua",
 	cmd = "Copilot",
 	event = "InsertEnter",
-	dependencies = { "zbirenbaum/copilot-cmp" },
 	config = function()
+		-- Define a global variable for Copilot toggle
+		_G.copilot_enabled = false
+
+		-- Function to toggle Copilot
+		function _G.toggle_copilot()
+			if _G.copilot_enabled then
+				vim.cmd("Copilot disable")
+				_G.copilot_enabled = false
+				print("Copilot disabled")
+			else
+				vim.cmd("Copilot enable")
+				_G.copilot_enabled = true
+				print("Copilot enabled")
+			end
+		end
+
+		-- Set up keybinding to toggle Copilot
+		vim.api.nvim_set_keymap("n", "<leader>cp", ":lua toggle_copilot()<CR>", { noremap = true, silent = true })
+
+		-- Copilot configuration
 		require("copilot").setup({
 			panel = {
 				enabled = true,
@@ -16,31 +35,36 @@ return {
 					open = "<leader>cpp",
 				},
 				layout = {
-					position = "right", -- | top | left | right | horizontal | vertical
+					position = "right",
 					ratio = 0.4,
 				},
 			},
 			suggestion = {
-				enabled = false, -- Disable suggestions to avoid conflicts with `cmp`
+				enabled = true,
 				auto_trigger = true,
 				debounce = 75,
+				keymap = {
+					accept = "<tab>",
+					accept_word = false,
+					accept_line = false,
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<C-]>",
+				},
 			},
 			filetypes = {
-				yaml = false,
-				markdown = false,
-				help = false,
-				gitcommit = false,
-				gitrebase = false,
-				hgcommit = false,
-				svn = false,
-				cvs = false,
-				["."] = false,
+				yaml = true,
+				markdown = true,
+				help = true,
+				gitcommit = true,
+				gitrebase = true,
+				hgcommit = true,
+				svn = true,
+				cvs = true,
+				["."] = true,
 			},
 			copilot_node_command = "node", -- Node.js version must be > 18.x
 			server_opts_overrides = {},
 		})
-
-		-- Setup copilot-cmp integration
-		require("copilot_cmp").setup()
 	end,
 }
