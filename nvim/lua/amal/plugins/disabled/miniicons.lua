41e369053e7710f3a0886307638055a931534e8b
Amal Lalgi <amallalgi2487@gmail.com>
2024-12-29 17:10:49 +0530

    refactor: disable unwanted plugins

--- nvim/lua/amal/plugins/disabled/miniicons.lua
+++ nvim/lua/amal/plugins/disabled/miniicons.lua
@@ -1,7 +1,15 @@
 return {
 	"echasnovski/mini.icons",
-	version = "*",
+	priority = 100,
+	lazy = false,
 	config = function()
-		require("mini.icons").setup()
+		require("mini.icons").setup({
+			lsp = {
+				copilot = { glyph = "", hl = "MiniIconsRed" },
+				signature = { glyph = "󰅲", hl = "MiniIconsBlue" },
+				-- supermaven = { glyph = "󰌶", hl = "MiniIconsRed" },
+			},
+		})
+		MiniIcons.mock_nvim_web_devicons()
 	end,
 }
--- nvim/lua/amal/plugins/disabled/whichkey.lua
+++ nvim/lua/amal/plugins/disabled/whichkey.lua
@@ -1,18 +1,14 @@
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
+	keys = require("mappings").which_key,
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 500
+	end,
 	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
-	keys = {
-		{
-			"<leader>?",
-			function()
-				require("which-key").show({ global = false })
-			end,
-			desc = "Buffer Local Keymaps (which-key)",
+		layout = {
+			height = { min = 4, max = 40 }, -- min and max height of the columns
 		},
 	},
 }
