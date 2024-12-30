b020204d9ba3f82622b998a32a5ada67b05fafcb
Amal Lalgi <amallalgi2487@gmail.com>
2024-12-31 01:15:22 +0530

    feat(autocmd): add commands to close filetypes with `q`

--- nvim/lua/amal/core/autocmd.lua
+++ nvim/lua/amal/core/autocmd.lua
@@ -23,3 +23,36 @@ api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 		vim.opt.spelllang = "en,de"
 	end,
 })
+
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
+	pattern = {
+		"PlenaryTestPopup",
+		"help",
+		"lspinfo",
+		"man",
+		"notify",
+		"qf",
+		"spectre_panel",
+		"startuptime",
+		"tsplayground",
+		"neotest-output",
+		"checkhealth",
+		"neotest-summary",
+		"neotest-output-panel",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+	end,
+})
+
+-- enable spell checking for certain file types
+api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = { "*.txt", "*.md", "*.tex" },
+	callback = function()
+		vim.opt.spell = true
+		vim.opt.spelllang = "en,de"
+	end,
+})
