73d7c76c027829c40fe89dc6af7ea363db5d1951
Amal Lalgi <amallalgi2487@gmail.com>
2025-01-01 14:24:50 +0530

    refactor: remove unwanted files and add more lsp servers

--- nvim/lua/amal/plugins/lsp/servers.lua
+++ nvim/lua/amal/plugins/lsp/servers.lua
@@ -1,53 +1,38 @@
 return {
-	jsonls = {
-		settings = {
-			json = {
-				schema = require("schemastore").json.schemas(),
-				validate = { enable = true },
-			},
-		},
-	},
-	terraformls = {
-		cmd = { "terraform-ls" },
-		arg = { "server" },
-		filetypes = { "terraform", "tf", "terraform-vars" },
-	},
-	lua_ls = {
-		settings = {
-			Lua = {
-				runtime = { version = "LuaJIT" },
-				workspace = {
-					checkThirdParty = false,
-					library = {
-						"${3rd}/luv/library",
-						unpack(vim.api.nvim_get_runtime_file("", true)),
-					},
-				},
-				completion = {
-					callSnippet = "Replace",
-				},
-			},
-		},
-	},
-	bashls = {
-		filetypes = { "sh", "zsh" },
-	},
-	vimls = {
-		filetypes = { "vim" },
-	},
-	-- tsserver = {},
-	ts_ls = {},
-	gopls = {},
-	pyright = {},
-
-	solidity_ls_nomicfoundation = {},
-	yamlls = {
-		cmd = { "yaml-language-server", "--stdio" },
-		filetypes = { "yaml" },
-	},
-
-	zls = {},
-	clangd = {},
-	marksman = {},
-	-- "markdownlint-cli2", "markdown-toc"
+  pyright = {},
+  ruff = {},
+  ts_ls = {},
+  cssls = {},
+  emmet_ls = {},
+  html = {},
+  tailwindcss = {},
+  rust_analyzer = {},
+  gopls = {},
+  clangd = {},
+  bashls = { filetypes = { "sh", "zsh" } },
+  jsonls = {
+    settings = {
+      json = {
+        schema = require("schemastore").json.schemas(),
+        validate = { enable = true },
+      },
+    },
+  },
+  lua_ls = {
+    settings = {
+      Lua = {
+        runtime = { version = "LuaJIT" },
+        workspace = {
+          checkThirdParty = false,
+          library = {
+            "${3rd}/luv/library",
+            unpack(vim.api.nvim_get_runtime_file("", true)),
+          },
+        },
+        completion = {
+          callSnippet = "Replace",
+        },
+      },
+    },
+  },
 }
