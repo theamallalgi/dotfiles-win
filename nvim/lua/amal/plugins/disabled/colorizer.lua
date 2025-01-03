4a97e81d251e369eb391befcb8a60465995905c7
Amal Lalgi <amallalgi2487@gmail.com>
2025-01-03 21:04:17 +0530

    chore: disable redundant plugins

--- nvim/lua/amal/plugins/disabled/colorizer.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"norcalli/nvim-colorizer.lua",
-	lazyload = true,
-	config = function()
-		require("colorizer").setup({
-			"*",
-		})
-	end,
-}
--- nvim/lua/amal/plugins/disabled/comment.lua
+++
@@ -1,6 +0,0 @@
-return {
-	'numToStr/Comment.nvim',
-	lazyload=true,
-	event = {"BufReadPre", "BufNewFile"},
-	config = true -- runs require('comment').setup()
-}
