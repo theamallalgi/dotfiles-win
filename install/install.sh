ee25409b62a935b16d3645ed7e2435556687dbca
Amal Lalgi <amallalgi2487@gmail.com>
2025-01-04 22:55:33 +0530

    fix: clear improper carriage returns

--- install/install.sh
+++ install/install.sh
@@ -8,36 +8,36 @@ PIP_SCRIPT="pip.sh"
 
 # run script and check for its existence
 run_script() {
-    local script=$1
-    if [[ -f "$script" ]]; then
-        echo -e "\n[i] running $script..."
-        bash "$script"
-    else
-        echo "[!] $script not found. please ensure it exists in the current directory."
-        exit 1
-    fi
+	local script=$1
+	if [[ -f "$script" ]]; then
+		echo -e "\n[i] running $script..."
+		bash "$script"
+	else
+		echo "[!] $script not found. please ensure it exists in the current directory."
+		exit 1
+	fi
 }
 
 # ask for confirmation before running a script
 run_script_with_query() {
-    local script=$1
-    local query=$2
-    if [[ -f "$script" ]]; then
-        echo -e "\n[?] $query"
-        read -r response
-        case "$response" in
-            [Yy]* | "")
-                echo "[i] running $script..."
-                bash "$script"
-                ;;
-            *)
-                echo "[>] skipping $script."
-                ;;
-        esac
-    else
-        echo "[!] $script not found. please ensure it exists in the current directory."
-        exit 1
-    fi
+	local script=$1
+	local query=$2
+	if [[ -f "$script" ]]; then
+		echo -e "\n[?] $query"
+		read -r response
+		case "$response" in
+		[Yy]* | "")
+			echo "[i] running $script..."
+			bash "$script"
+			;;
+		*)
+			echo "[>] skipping $script."
+			;;
+		esac
+	else
+		echo "[!] $script not found. please ensure it exists in the current directory."
+		exit 1
+	fi
 }
 
 chmod +x scoop.sh pip.sh # grant required permissions to files
