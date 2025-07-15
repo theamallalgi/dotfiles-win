fa999e0b6b8aeb0727eae1956d41d21cddd00d94
Amal Lalgi <amallalgi2487@gmail.com>
2025-07-15 20:33:36 +0530

    feat(textobjects): add text-obj functionality for lsp based editing
    - improved visual selection for languages like python

--- nvim/lua/amal/plugins/treesitter.lua
+++ nvim/lua/amal/plugins/treesitter.lua
@@ -1,12 +1,27 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = {"lua", "javascript", "python"},
+			ensure_installed = { "lua", "javascript", "python" },
 			highlight = { enable = true },
-			indent = { enable = true }
+			indent = { enable = true },
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+						["a/"] = "@comment.outer",
+						["i/"] = "@comment.inner",
+					},
+				},
+			},
 		})
-	end
+	end,
 }
