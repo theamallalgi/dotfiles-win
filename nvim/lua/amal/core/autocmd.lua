66c4f0998e11ca61cf28334461c01c54caf1ea6c
Amal Lalgi <amallalgi2487@gmail.com>
2025-07-15 20:30:10 +0530

    feat(autocmd): add warning message when using arrow keys in `nor` and `ins`

--- nvim/lua/amal/core/autocmd.lua
+++ nvim/lua/amal/core/autocmd.lua
@@ -1,5 +1,6 @@
 -- autocmd.lua - commands and automations to run with nvim
 
+local vim = vim
 local api = vim.api
 
 -- don't auto comment new line
@@ -19,33 +20,54 @@ api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
 
 -- close some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
-  group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
-  pattern = {
-    "PlenaryTestPopup",
-    "help",
-    "lspinfo",
-    "man",
-    "notify",
-    "qf",
-    "spectre_panel",
-    "startuptime",
-    "tsplayground",
-    "neotest-output",
-    "checkhealth",
-    "neotest-summary",
-    "neotest-output-panel",
-  },
-  callback = function(event)
-    vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
-  end,
+	group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
+	pattern = {
+		"PlenaryTestPopup",
+		"help",
+		"lspinfo",
+		"man",
+		"notify",
+		"qf",
+		"spectre_panel",
+		"startuptime",
+		"tsplayground",
+		"neotest-output",
+		"checkhealth",
+		"neotest-summary",
+		"neotest-output-panel",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+	end,
 })
 
 -- enable spell checking for certain file types
 api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = { "*.txt", "*.md", "*.tex" },
-  callback = function()
-    vim.opt.spell = true
-    vim.opt.spelllang = "en,de"
-  end,
+	pattern = { "*.txt", "*.md", "*.tex" },
+	callback = function()
+		vim.opt.spell = true
+		vim.opt.spelllang = "en,de"
+	end,
+})
+
+--  warning message when using the arrow keys
+api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		local warn = function()
+			vim.notify("Nuh uh uh! Use the hjkl keys buddy!", vim.log.levels.WARN)
+		end
+
+		local opts = { noremap = true, silent = true }
+
+		vim.keymap.set("n", "<Up>", warn, opts)
+		vim.keymap.set("n", "<Down>", warn, opts)
+		vim.keymap.set("n", "<Left>", warn, opts)
+		vim.keymap.set("n", "<Right>", warn, opts)
+
+		vim.keymap.set("i", "<Up>", warn, opts)
+		vim.keymap.set("i", "<Down>", warn, opts)
+		vim.keymap.set("i", "<Left>", warn, opts)
+		vim.keymap.set("i", "<Right>", warn, opts)
+	end,
 })
